83905152a04caa38ba5582fdbdf2444fa40a5cf8
Nishit <nishitgajjar1999@gmail.com>
2020-05-20 18:02:01 +0530

    Chat App

--- app/src/main/java/com/thenextlevel/flashchat/MainActivity.java
+++ app/src/main/java/com/thenextlevel/flashchat/MainActivity.java
@@ -7,10 +7,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
